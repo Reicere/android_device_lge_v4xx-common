7d2837f05ae923501402636d6bb6377e672d0188
Adam Myczkowski <mycaxd511@gmail.com>
2017-07-04 07:48:28 -0400

    Revert "v4xx: Shim libqc-opt"
    This reverts commit b9fb79b5af60da7d9768941b970de2c48de97667.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -25,14 +25,3 @@ LOCAL_MODULE := libshims_wvm
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    hw_utext.c
-
-LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
-LOCAL_MODULE := libhw_utext
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
